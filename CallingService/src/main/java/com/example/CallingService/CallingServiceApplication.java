f287a6fdac346cc98dc7f1e64a43382b182fdf29
Anup Kuber Mohite <46156833+anupmohite41@users.noreply.github.com>
2019-04-24 11:24:30 +0530

    Update CallingServiceApplication.java

--- CallingService/src/main/java/com/example/CallingService/CallingServiceApplication.java
+++ CallingService/src/main/java/com/example/CallingService/CallingServiceApplication.java
@@ -10,6 +10,7 @@ public class CallingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CallingServiceApplication.class, args);
+		System.out.println("Applcation Started");
 	}
 
 }
